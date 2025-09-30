2fdbf024fa9233c7c46db84bca2ba806aef07f4b
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-09-30 13:13:47 +0530

    add /

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(express.json());
 // Routes
 app.use("/search", searchRoutes);
 
+// Default route
+app.use("/", (req, res) => {
+    res.send("API working");
+  });
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
